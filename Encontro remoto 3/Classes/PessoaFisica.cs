42daa9f6da56f45331ce4ff66d0e672dc8c657aa
Diegocpx <107122227+Diegocpx@users.noreply.github.com>
2022-08-18 21:53:51 -0300

    codificacaoBackend

---
+++ Encontro remoto 3/Classes/PessoaFisica.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Encontro_remoto_2.Interfaces;
+
+namespace Encontro_remoto_2.Classes
+{
+    public class PessoaFisica : Pessoa , IPessoaFisica
+    {
+        public string? cpf { get; set; }
+        
+        public DateTime DataNasc { get; set; }
+
+        public override float CalcularImposto(float rendimento)
+        {
+            if (rendimento <= 1500)
+            {
+                return 0;
+            }else if (rendimento > 1500 && rendimento <= 3500)
+            {
+                float resultado = (rendimento / 100) * 2;
+
+                return resultado;
+
+            }else if (rendimento > 3500 && rendimento <=6000)
+            {
+                float resultado = (rendimento / 100) * 3.5f;
+
+                return resultado;
+            }else
+            {
+                float resultado = (rendimento / 100) * 5;
+
+                return resultado;
+            }
+        }
+
+
+        public bool ValidarDataNasc(DateTime DataNasc)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Encontro remoto 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using Encontro_remoto_2.Classes;
+
+PessoaFisica novaPF = new PessoaFisica();
+
+
+novaPF.nome = "Diego";
+novaPF.cpf = "12345678910";
+novaPF.rendimento = 6600.5f;
+
+// Console.WriteLine(novaPF.nome);
+
+// Console.WriteLine("Nome: " + novaPF.nome + " - CPF: " + novaPF.cpf);
+// Console.WriteLine($"Nome: {novaPF.nome} - CPF: {novaPF.cpf}");
+
+float impostoPagar = novaPF.CalcularImposto(novaPF.rendimento);
+Console.WriteLine(impostoPagar);
+Console.WriteLine($"R$ {impostoPagar:0.00}");
+Console.WriteLine(impostoPagar.ToString("C"));
+
+PessoaJuridica novaPJ = new PessoaJuridica();
+Console.WriteLine(novaPJ.CalcularImposto(6600.5f));
